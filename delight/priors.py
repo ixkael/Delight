d61a26022c72ef3e575027eff04f9b0b91d533ad
Boris Leistedt <boris.leistedt@gmail.com>
2016-06-03 23:47:13 +0100

    Added Rayleigh distribution, gradients and tests

--- delight/priors.py
+++ delight/priors.py
@@ -5,6 +5,7 @@ from GPy.core.parameterization.priors import Prior
 import weakref
 from scipy.special import gamma, gammaln, polygamma
 
+
 class Schechter(Prior):
     """
     Schechter luminosity function (normalized)
@@ -106,3 +107,60 @@ class Kumaraswamy(Prior):
     def lnpdf_grad_alpha1(self, t):
         """Derivative of lnprob with respect to alpha1"""
         return 1/self.alpha1 + np.log(1 - t**self.alpha0)
+
+
+
+class Rayleigh(Prior):
+    """
+    Rayleigh distribution
+        p(z|t) = exp(-0.5 * z^2 / alpha(t)^2) / alpha(t)^2
+        with alpha(t) = alpha0 + alpha1 * t
+    """
+    domain = _REAL
+    _instances = []
+
+    def __new__(cls, alpha0=1.0, alpha1=1.0):  # Singleton:
+        if cls._instances:
+            cls._instances[:] = [instance for instance in cls._instances if instance()]
+            for instance in cls._instances:
+                if instance().alpha0 == alpha0 and instance().alpha1 == alpha1:
+                    return instance()
+        newfunc = super(Prior, cls).__new__
+        if newfunc is object.__new__:
+            o = newfunc(cls)
+        else:
+            o = newfunc(cls, mu, sigma)
+        cls._instances.append(weakref.ref(o))
+        return cls._instances[-1]()
+
+    def __init__(self, alpha0, alpha1):
+        self.alpha0 = float(alpha0)
+        self.alpha1 = float(alpha1)
+
+    def __str__(self):
+        return "Rayleigh({:.2g}, {:.2g})".format(self.alpha0, self.alpha1)
+
+    def lnpdf(self, z, t):
+        """Lnprob"""
+        alpha2 = (self.alpha0 + self.alpha1 * t)**2.0
+        return - alpha2 + np.log(z) - z**2 / 2 / alpha2
+
+    def lnpdf_grad_z(self, z, t):
+        """Derivative of lnprob with respect to z"""
+        alpha2 = (self.alpha0 + self.alpha1 * t)**2
+        return 1.0 / z - z / alpha2
+
+    def lnpdf_grad_t(self, z, t):
+        """Derivative of lnprob with respect to t"""
+        alpha = (self.alpha0 + self.alpha1 * t)
+        return -2 * self.alpha1 * alpha + self.alpha1 * z**2 / alpha**3
+
+    def lnpdf_grad_alpha0(self, z, t):
+        """Derivative of lnprob with respect to alpha0"""
+        alpha = (self.alpha0 + self.alpha1 * t)
+        return -2 * alpha + z**2 / alpha**3
+
+    def lnpdf_grad_alpha1(self, z, t):
+        """Derivative of lnprob with respect to alpha1"""
+        alpha = (self.alpha0 + self.alpha1 * t)
+        return -2 * t * alpha + t * z**2 / alpha**3
--- tests/test_priors.py
+++ tests/test_priors.py
@@ -23,28 +23,28 @@ def test_schechter_derivatives():
         def f_ell(ell, t, ellStar, alpha0, alpha1):
             dist2 = Schechter(ellStar, alpha0, alpha1)
             return dist2.lnpdf(ell, t)
-        v2 = derivative(f_ell, ell, dx=0.1, n=1, args=(t, ellStar, alpha0, alpha1), order=5)
+        v2 = derivative(f_ell, ell, dx=0.01, n=1, args=(t, ellStar, alpha0, alpha1), order=5)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_t(ell, t)
         def f_t(t, ell, ellStar, alpha0, alpha1):
             dist2 = Schechter(ellStar, alpha0, alpha1)
             return dist2.lnpdf(ell, t)
-        v2 = derivative(f_t, t, dx=0.1, n=1, args=(ell, ellStar, alpha0, alpha1), order=5)
+        v2 = derivative(f_t, t, dx=0.01, n=1, args=(ell, ellStar, alpha0, alpha1), order=5)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_alpha0(ell, t)
         def f_alpha0(alpha0, t, ell, ellStar, alpha1):
             dist2 = Schechter(ellStar, alpha0, alpha1)
             return dist2.lnpdf(ell, t)
-        v2 = derivative(f_alpha0, alpha0, dx=0.05, n=1, args=(t, ell, ellStar, alpha1), order=5)
+        v2 = derivative(f_alpha0, alpha0, dx=0.01, n=1, args=(t, ell, ellStar, alpha1), order=5)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_alpha1(ell, t)
         def f_alpha1(alpha1, t, ell, ellStar, alpha0):
             dist2 = Schechter(ellStar, alpha0, alpha1)
             return dist2.lnpdf(ell, t)
-        v2 = derivative(f_alpha1, alpha1, dx=0.05, n=1, args=(t, ell, ellStar, alpha0), order=5)
+        v2 = derivative(f_alpha1, alpha1, dx=0.01, n=1, args=(t, ell, ellStar, alpha0), order=5)
         assert abs(v1/v2-1) < 0.01
 
 
@@ -80,4 +80,48 @@ def test_kumaraswamy_derivatives():
             dist2 = Kumaraswamy(alpha0, alpha1)
             return dist2.lnpdf(t)
         v2 = derivative(f_alpha1, alpha1, dx=0.01, n=1, args=(t, alpha0), order=5)
+        assert abs(v1/v2-1) < 0.01
+
+
+
+def test_rayleigh_derivatives():
+    """
+    Numerically test derivatives of Rayleigh distribution
+    one by one for a few random parameters and values
+    """
+    size = 10
+    for i in range(size):
+        z = np.random.uniform(low=0., high=3.0, size=1)
+        t = np.random.uniform(low=0, high=1.0, size=1)
+        alpha0 = np.random.uniform(low=0.2, high=2.0, size=1)
+        alpha1 = np.random.uniform(low=0.2, high=2.0, size=1)
+        alpha = alpha0 + t* alpha1
+        dist = Rayleigh(alpha0, alpha1)
+
+        v1 = dist.lnpdf_grad_z(z, t)
+        def f_z(z, t, alpha0, alpha1):
+            dist2 = Rayleigh(alpha0, alpha1)
+            return dist2.lnpdf(z, t)
+        v2 = derivative(f_z, z, dx=0.01, n=1, args=(t, alpha0, alpha1), order=5)
+        assert abs(v1/v2-1) < 0.01
+
+        v1 = dist.lnpdf_grad_t(z, t)
+        def f_t(t, z, alpha0, alpha1):
+            dist2 = Rayleigh(alpha0, alpha1)
+            return dist2.lnpdf(z, t)
+        v2 = derivative(f_t, t, dx=0.01, n=1, args=(z, alpha0, alpha1), order=5)
+        assert abs(v1/v2-1) < 0.01
+
+        v1 = dist.lnpdf_grad_alpha0(z, t)
+        def f_alpha0(alpha0, t, z, alpha1):
+            dist2 = Rayleigh(alpha0, alpha1)
+            return dist2.lnpdf(z, t)
+        v2 = derivative(f_alpha0, alpha0, dx=0.01, n=1, args=(t, z, alpha1), order=5)
+        assert abs(v1/v2-1) < 0.01
+
+        v1 = dist.lnpdf_grad_alpha1(z, t)
+        def f_alpha1(alpha1, t, z, alpha0):
+            dist2 = Rayleigh(alpha0, alpha1)
+            return dist2.lnpdf(z, t)
+        v2 = derivative(f_alpha1, alpha1, dx=0.01, n=1, args=(t, z, alpha0), order=5)
         assert abs(v1/v2-1) < 0.01
