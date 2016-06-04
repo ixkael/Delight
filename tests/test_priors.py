0d7cc928cf3453a90bae81f4ec58f178f8f077c8
Boris Leistedt <boris.leistedt@gmail.com>
2016-06-04 12:07:24 +0100

    Improved test setup to avoid random walk outside of allowed space

--- tests/test_priors.py
+++ tests/test_priors.py
@@ -2,13 +2,14 @@
 from delight.priors import *
 from scipy.misc import derivative
 
+NREPEAT = 10
+
 def test_schechter_derivatives():
     """
     Numerically test derivatives of Schechter distribution
     one by one for a few random parameters and values
     """
-    size = 10
-    for i in range(size):
+    for i in range(NREPEAT):
         ellStar = np.random.uniform(low=0., high=10.0, size=1)
         ell = np.random.uniform(low=0., high=10.0, size=1)
         t = np.random.uniform(low=0, high=1.0, size=1)
@@ -23,28 +24,28 @@ def test_schechter_derivatives():
         def f_ell(ell, t, ellStar, alpha0, alpha1):
             dist2 = Schechter(ellStar, alpha0, alpha1)
             return dist2.lnpdf(ell, t)
-        v2 = derivative(f_ell, ell, dx=0.01, n=1, args=(t, ellStar, alpha0, alpha1), order=5)
+        v2 = derivative(f_ell, ell, dx=0.01*ell, n=1, args=(t, ellStar, alpha0, alpha1), order=5)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_t(ell, t)
         def f_t(t, ell, ellStar, alpha0, alpha1):
             dist2 = Schechter(ellStar, alpha0, alpha1)
             return dist2.lnpdf(ell, t)
-        v2 = derivative(f_t, t, dx=0.01, n=1, args=(ell, ellStar, alpha0, alpha1), order=5)
+        v2 = derivative(f_t, t, dx=0.01*t, n=1, args=(ell, ellStar, alpha0, alpha1), order=5)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_alpha0(ell, t)
         def f_alpha0(alpha0, t, ell, ellStar, alpha1):
             dist2 = Schechter(ellStar, alpha0, alpha1)
             return dist2.lnpdf(ell, t)
-        v2 = derivative(f_alpha0, alpha0, dx=0.01, n=1, args=(t, ell, ellStar, alpha1), order=5)
+        v2 = derivative(f_alpha0, alpha0, dx=0.01*alpha0, n=1, args=(t, ell, ellStar, alpha1), order=5)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_alpha1(ell, t)
         def f_alpha1(alpha1, t, ell, ellStar, alpha0):
             dist2 = Schechter(ellStar, alpha0, alpha1)
             return dist2.lnpdf(ell, t)
-        v2 = derivative(f_alpha1, alpha1, dx=0.01, n=1, args=(t, ell, ellStar, alpha0), order=5)
+        v2 = derivative(f_alpha1, alpha1, dx=0.01*alpha1, n=1, args=(t, ell, ellStar, alpha0), order=5)
         assert abs(v1/v2-1) < 0.01
 
 
@@ -54,8 +55,7 @@ def test_kumaraswamy_derivatives():
     Numerically test derivatives of Kumaraswamy distribution
     one by one for a few random parameters and values
     """
-    size = 10
-    for i in range(size):
+    for i in range(NREPEAT):
         t = np.random.uniform(low=0.1, high=0.9, size=1)
         alpha0 = np.random.uniform(low=0.2, high=10.0, size=1)
         alpha1 = np.random.uniform(low=0.2, high=10.0, size=1)
@@ -65,21 +65,21 @@ def test_kumaraswamy_derivatives():
         def f_t(t, alpha0, alpha1):
             dist2 = Kumaraswamy(alpha0, alpha1)
             return dist2.lnpdf(t)
-        v2 = derivative(f_t, t, dx=0.01, n=1, args=(alpha0, alpha1), order=5)
+        v2 = derivative(f_t, t, dx=0.01*t, n=1, args=(alpha0, alpha1), order=5)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_alpha0(t)
         def f_alpha0(alpha0, t, alpha1):
             dist2 = Kumaraswamy(alpha0, alpha1)
             return dist2.lnpdf(t)
-        v2 = derivative(f_alpha0, alpha0, dx=0.01, n=1, args=(t, alpha1), order=5)
+        v2 = derivative(f_alpha0, alpha0, dx=0.01*alpha0, n=1, args=(t, alpha1), order=5)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_alpha1(t)
         def f_alpha1(alpha1, t, alpha0):
             dist2 = Kumaraswamy(alpha0, alpha1)
             return dist2.lnpdf(t)
-        v2 = derivative(f_alpha1, alpha1, dx=0.01, n=1, args=(t, alpha0), order=5)
+        v2 = derivative(f_alpha1, alpha1, dx=0.01*alpha1, n=1, args=(t, alpha0), order=5)
         assert abs(v1/v2-1) < 0.01
 
 
@@ -89,8 +89,7 @@ def test_rayleigh_derivatives():
     Numerically test derivatives of Rayleigh distribution
     one by one for a few random parameters and values
     """
-    size = 10
-    for i in range(size):
+    for i in range(NREPEAT):
         z = np.random.uniform(low=0., high=3.0, size=1)
         t = np.random.uniform(low=0, high=1.0, size=1)
         alpha0 = np.random.uniform(low=0.2, high=2.0, size=1)
@@ -102,26 +101,26 @@ def test_rayleigh_derivatives():
         def f_z(z, t, alpha0, alpha1):
             dist2 = Rayleigh(alpha0, alpha1)
             return dist2.lnpdf(z, t)
-        v2 = derivative(f_z, z, dx=0.01, n=1, args=(t, alpha0, alpha1), order=5)
+        v2 = derivative(f_z, z, dx=0.01*z, n=1, args=(t, alpha0, alpha1), order=5)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_t(z, t)
         def f_t(t, z, alpha0, alpha1):
             dist2 = Rayleigh(alpha0, alpha1)
             return dist2.lnpdf(z, t)
-        v2 = derivative(f_t, t, dx=0.01, n=1, args=(z, alpha0, alpha1), order=5)
+        v2 = derivative(f_t, t, dx=0.01*t, n=1, args=(z, alpha0, alpha1), order=5)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_alpha0(z, t)
         def f_alpha0(alpha0, t, z, alpha1):
             dist2 = Rayleigh(alpha0, alpha1)
             return dist2.lnpdf(z, t)
-        v2 = derivative(f_alpha0, alpha0, dx=0.01, n=1, args=(t, z, alpha1), order=5)
+        v2 = derivative(f_alpha0, alpha0, dx=0.01*alpha0, n=1, args=(t, z, alpha1), order=5)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_alpha1(z, t)
         def f_alpha1(alpha1, t, z, alpha0):
             dist2 = Rayleigh(alpha0, alpha1)
             return dist2.lnpdf(z, t)
-        v2 = derivative(f_alpha1, alpha1, dx=0.01, n=1, args=(t, z, alpha0), order=5)
+        v2 = derivative(f_alpha1, alpha1, dx=0.01*alpha1, n=1, args=(t, z, alpha0), order=5)
         assert abs(v1/v2-1) < 0.01
