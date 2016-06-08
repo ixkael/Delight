558a8d2cd08585f263399de683e546a7e9baa0d9
Boris Leistedt <boris.leistedt@gmail.com>
2016-06-08 09:20:56 -0400

    Simplified priors and parameterized them to link them with models

--- delight/priors.py
+++ delight/priors.py
@@ -1,115 +1,28 @@
 
 import numpy as np
 from paramz.domains import _REAL
-from GPy.core.parameterization.priors import Prior
+from GPy.core.parameterization.priors import Prior, Parameterized, Param
 import weakref
 from scipy.special import gamma, gammaln, polygamma
 
 
-class FullZTLPrior(Prior):
-    """
-    Full prior p(z,ell,t), combining
-    Rayleigh, Schechter, and Kumaraswamy distributions
-    """
-
-    domain = _REAL
-    _instances = []
-
-    def __new__(cls, L_ellStar=1.0, L_alpha0=-0.5, L_alpha1=0.1,
-                T_alpha0=1.0, T_alpha1=1.0,
-                Z_alpha0=1.0, Z_alpha1=1.0):  # Singleton:
-        if cls._instances:
-            cls._instances[:]\
-                = [instance for instance in cls._instances if instance()]
-            for instance in cls._instances:
-                if instance().L_ellStar == L_ellStar\
-                    and instance().L_alpha0 == L_alpha0\
-                        and instance().L_alpha1 == L_alpha1\
-                        and instance().T_alpha0 == L_alpha0\
-                        and instance().L_alpha1 == L_alpha1\
-                        and instance().Z_alpha0 == Z_alpha0\
-                        and instance().Z_alpha1 == Z_alpha1:
-                    return instance()
-        newfunc = super(Prior, cls).__new__
-        if newfunc is object.__new__:
-            o = newfunc(cls)
-        else:
-            o = newfunc(cls, L_ellStar, L_alpha0, L_alpha1,
-                        T_alpha0, T_alpha1,
-                        Z_alpha0, Z_alpha1)
-        cls._instances.append(weakref.ref(o))
-        return cls._instances[-1]()
-
-    def __init__(self,
-                 L_ellStar, L_alpha0, L_alpha1,
-                 T_alpha0, T_alpha1,
-                 Z_alpha0, Z_alpha1
-                 ):
-        self.p_z_t = Rayleigh(Z_alpha0, Z_alpha1)
-        self.p_l_t = Schechter(L_ellStar, L_alpha0, L_alpha1)
-        self.p_t = Kumaraswamy(T_alpha0, T_alpha1)
-        self.L_ellStar = self.p_l_t.ellStar
-        self.L_alpha0 = self.p_l_t.alpha0
-        self.L_alpha1 = self.p_l_t.alpha1
-        self.T_alpha0 = self.p_t.alpha0
-        self.T_alpha1 = self.p_t.alpha1
-        self.Z_alpha0 = self.p_z_t.alpha0
-        self.Z_alpha1 = self.p_z_t.alpha1
-
-    def lnpdf(self, z, ell, t):
-        """Full lnprob"""
-        return self.p_z_t.lnpdf(z, t) +\
-            self.p_l_t.lnpdf(ell, t) +\
-            self.p_t.lnpdf(t)
-
-    def lnpdf_grad_ell(self, z, ell, t):
-        """Derivative of lnprob with respect to ell"""
-        return self.p_l_t.lnpdf_grad_ell(ell, t)
-
-    def lnpdf_grad_t(self, z, ell, t):
-        """Derivative of lnprob with respect to t"""
-        return self.p_z_t.lnpdf_grad_t(z, t) +\
-            self.p_l_t.lnpdf_grad_t(ell, t) +\
-            self.p_t.lnpdf_grad_t(t)
-
-    def lnpdf_grad_z(self, z, ell, t):
-        """Derivative of lnprob with respect to t"""
-        return self.p_z_t.lnpdf_grad_z(z, t)
-
-    # TODO: add gradients, in separate or combined function
-
-
-class Schechter(Prior):
+class Schechter(Parameterized):
     """
     Schechter luminosity function (normalized)
         p(l|t) = (l/l*)^alpha(t) * exp(l/l*) / l* / Gamma(alpha(t)+1)
         with alpha(t) = alpha0 + alpha1 * t
     """
     domain = _REAL
-    _instances = []
 
-    def __new__(cls, ellStar=1.0, alpha0=-0.5, alpha1=0.1):  # Singleton:
-        if cls._instances:
-            cls._instances[:]\
-                = [instance for instance in cls._instances if instance()]
-            for instance in cls._instances:
-                if instance().ellStar == ellStar\
-                    and instance().alpha0 == alpha0\
-                        and instance().alpha1 == alpha1:
-                    return instance()
-        newfunc = super(Prior, cls).__new__
-        if newfunc is object.__new__:
-            o = newfunc(cls)
-        else:
-            o = newfunc(cls, ellStar, alpha0, alpha1)
-        cls._instances.append(weakref.ref(o))
-        return cls._instances[-1]()
-
-    def __init__(self, ellStar, alpha0, alpha1):
-        self.ellStar = float(ellStar)
+    def __init__(self, ellStar, alpha0, alpha1, name='Schechter'):
+        super(Schechter, self).__init__(name=name)
+        self.ellStar = Param('Schechter_ellStar', float(ellStar))
+        self.alpha0 = Param('Schechter_alpha0', float(alpha0))
+        self.alpha1 = Param('Schechter_alpha1', float(alpha1))
+        self.link_parameter(self.ellStar)
+        self.link_parameter(self.alpha0)
+        self.link_parameter(self.alpha1)
         self.lnEllStar = np.log(self.ellStar)
-        self.alpha0 = float(alpha0)
-        self.alpha1 = float(alpha1)
 
     def __str__(self):
         return "Schechter({:.2g}, {:.2g}, {:.2g})"\
@@ -141,32 +54,19 @@ class Schechter(Prior):
                     polygamma(0, 1 + self.alpha0 + self.alpha1 * t))
 
 
-class Kumaraswamy(Prior):
+class Kumaraswamy(Parameterized):
     """
     Kumaraswamy distribution
         p(t) = alpha0 * alpha1 * t^(alpha0-1) * (1-t^alpha0)^(alpha1-1)
     """
     domain = _REAL
-    _instances = []
 
-    def __new__(cls, alpha0=1.0, alpha1=1.0):  # Singleton:
-        if cls._instances:
-            cls._instances[:]\
-                = [instance for instance in cls._instances if instance()]
-            for instance in cls._instances:
-                if instance().alpha0 == alpha0 and instance().alpha1 == alpha1:
-                    return instance()
-        newfunc = super(Prior, cls).__new__
-        if newfunc is object.__new__:
-            o = newfunc(cls)
-        else:
-            o = newfunc(cls, alpha0, alpha1)
-        cls._instances.append(weakref.ref(o))
-        return cls._instances[-1]()
-
-    def __init__(self, alpha0, alpha1):
-        self.alpha0 = float(alpha0)
-        self.alpha1 = float(alpha1)
+    def __init__(self, alpha0, alpha1, name='Kumaraswamy'):
+        super(Kumaraswamy, self).__init__(name=name)
+        self.alpha0 = Param('Kumaraswamy_alpha0', float(alpha0))
+        self.alpha1 = Param('Kumaraswamy_alpha1', float(alpha1))
+        self.link_parameter(self.alpha0)
+        self.link_parameter(self.alpha1)
         self.logalpha0 = np.log(alpha0)
         self.logalpha1 = np.log(alpha1)
 
@@ -195,33 +95,20 @@ class Kumaraswamy(Prior):
         return 1/self.alpha1 + np.log(1 - t**self.alpha0)
 
 
-class Rayleigh(Prior):
+class Rayleigh(Parameterized):
     """
     Rayleigh distribution
         p(z|t) = exp(-0.5 * z^2 / alpha(t)^2) / alpha(t)^2
         with alpha(t) = alpha0 + alpha1 * t
     """
     domain = _REAL
-    _instances = []
 
-    def __new__(cls, alpha0=1.0, alpha1=1.0):  # Singleton:
-        if cls._instances:
-            cls._instances[:]\
-                = [instance for instance in cls._instances if instance()]
-            for instance in cls._instances:
-                if instance().alpha0 == alpha0 and instance().alpha1 == alpha1:
-                    return instance()
-        newfunc = super(Prior, cls).__new__
-        if newfunc is object.__new__:
-            o = newfunc(cls)
-        else:
-            o = newfunc(cls, alpha0, alpha1)
-        cls._instances.append(weakref.ref(o))
-        return cls._instances[-1]()
-
-    def __init__(self, alpha0, alpha1):
-        self.alpha0 = float(alpha0)
-        self.alpha1 = float(alpha1)
+    def __init__(self, alpha0, alpha1, name='Rayleigh'):
+        super(Rayleigh, self).__init__(name)
+        self.alpha0 = Param('Rayleigh_alpha0', float(alpha0))
+        self.alpha1 = Param('Rayleigh_alpha1', float(alpha1))
+        self.link_parameter(self.alpha0)
+        self.link_parameter(self.alpha1)
 
     def __str__(self):
         return "Rayleigh({:.2g}, {:.2g})".format(self.alpha0, self.alpha1)
--- tests/test_priors.py
+++ tests/test_priors.py
@@ -5,54 +5,6 @@ from scipy.misc import derivative
 NREPEAT = 10
 
 
-def test_3dprior_derivatives():
-    """
-    Numerically test derivatives of the full 3D prior
-    one by one for a few random parameters and values
-    """
-    for i in range(NREPEAT):
-        L_ellStar = np.random.uniform(low=0., high=10.0, size=1)
-        z = np.random.uniform(low=0., high=3.0, size=1)
-        ell = np.random.uniform(low=0., high=10.0, size=1)
-        t = np.random.uniform(low=0.1, high=0.9, size=1)
-        T_alpha0 = np.random.uniform(low=0.2, high=10.0, size=1)
-        T_alpha1 = np.random.uniform(low=0.2, high=10.0, size=1)
-        L_alpha0 = np.random.uniform(low=-0.7, high=0.3, size=1)
-        L_alpha1 = np.random.uniform(low=0.1, high=0.2, size=1)
-        Z_alpha0 = np.random.uniform(low=0.2, high=2.0, size=1)
-        Z_alpha1 = np.random.uniform(low=0.2, high=2.0, size=1)
-
-        dist = FullZTLPrior(L_ellStar,
-                            L_alpha0, L_alpha1,
-                            T_alpha0, T_alpha1,
-                            Z_alpha0, Z_alpha1
-                            )
-
-        v1 = dist.lnpdf_grad_ell(z, ell, t)
-
-        def f_ell(ell, t, z):
-            return dist.lnpdf(z, ell, t)
-        v2 = derivative(f_ell, ell, dx=0.01*ell,
-                        args=(t, z), order=5)
-        assert abs(v1/v2-1) < 0.01
-
-        v1 = dist.lnpdf_grad_t(z, ell, t)
-
-        def f_t(t, ell, z):
-            return dist.lnpdf(z, ell, t)
-        v2 = derivative(f_t, t, dx=0.01*t,
-                        args=(ell, z), order=5)
-        assert abs(v1/v2-1) < 0.01
-
-        v1 = dist.lnpdf_grad_z(z, ell, t)
-
-        def f_z(z, t, ell):
-            return dist.lnpdf(z, ell, t)
-        v2 = derivative(f_z, z, dx=0.01*z,
-                        args=(t, ell), order=5)
-        assert abs(v1/v2-1) < 0.01
-
-
 def test_schechter_derivatives():
     """
     Numerically test derivatives of Schechter distribution
@@ -71,38 +23,34 @@ def test_schechter_derivatives():
 
         v1 = dist.lnpdf_grad_ell(ell, t)
 
-        def f_ell(ell, t, ellStar, alpha0, alpha1):
+        def f_ell(ell):
             dist2 = Schechter(ellStar, alpha0, alpha1)
             return dist2.lnpdf(ell, t)
-        v2 = derivative(f_ell, ell, dx=0.01*ell,
-                        args=(t, ellStar, alpha0, alpha1), order=5)
+        v2 = derivative(f_ell, ell, dx=0.01*ell)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_t(ell, t)
 
-        def f_t(t, ell, ellStar, alpha0, alpha1):
+        def f_t(t):
             dist2 = Schechter(ellStar, alpha0, alpha1)
             return dist2.lnpdf(ell, t)
-        v2 = derivative(f_t, t, dx=0.01*t,
-                        args=(ell, ellStar, alpha0, alpha1), order=5)
+        v2 = derivative(f_t, t, dx=0.01*t)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_alpha0(ell, t)
 
-        def f_alpha0(alpha0, t, ell, ellStar, alpha1):
+        def f_alpha0(alpha0):
             dist2 = Schechter(ellStar, alpha0, alpha1)
             return dist2.lnpdf(ell, t)
-        v2 = derivative(f_alpha0, alpha0, dx=0.01*alpha0,
-                        args=(t, ell, ellStar, alpha1), order=5)
+        v2 = derivative(f_alpha0, alpha0, dx=0.01*alpha0)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_alpha1(ell, t)
 
-        def f_alpha1(alpha1, t, ell, ellStar, alpha0):
+        def f_alpha1(alpha1):
             dist2 = Schechter(ellStar, alpha0, alpha1)
             return dist2.lnpdf(ell, t)
-        v2 = derivative(f_alpha1, alpha1, dx=0.01*alpha1,
-                        args=(t, ell, ellStar, alpha0), order=5)
+        v2 = derivative(f_alpha1, alpha1, dx=0.01*alpha1)
         assert abs(v1/v2-1) < 0.01
 
 
@@ -127,20 +75,18 @@ def test_kumaraswamy_derivatives():
 
         v1 = dist.lnpdf_grad_alpha0(t)
 
-        def f_alpha0(alpha0, t, alpha1):
+        def f_alpha0(alpha0):
             dist2 = Kumaraswamy(alpha0, alpha1)
             return dist2.lnpdf(t)
-        v2 = derivative(f_alpha0, alpha0, dx=0.01*alpha0,
-                        args=(t, alpha1), order=5)
+        v2 = derivative(f_alpha0, alpha0, dx=0.01*alpha0)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_alpha1(t)
 
-        def f_alpha1(alpha1, t, alpha0):
+        def f_alpha1(alpha1):
             dist2 = Kumaraswamy(alpha0, alpha1)
             return dist2.lnpdf(t)
-        v2 = derivative(f_alpha1, alpha1, dx=0.01*alpha1,
-                        args=(t, alpha0), order=5)
+        v2 = derivative(f_alpha1, alpha1, dx=0.01*alpha1)
         assert abs(v1/v2-1) < 0.01
 
 
@@ -159,34 +105,32 @@ def test_rayleigh_derivatives():
 
         v1 = dist.lnpdf_grad_z(z, t)
 
-        def f_z(z, t, alpha0, alpha1):
+        def f_z(z):
             dist2 = Rayleigh(alpha0, alpha1)
             return dist2.lnpdf(z, t)
-        v2 = derivative(f_z, z, dx=0.01*z, args=(t, alpha0, alpha1), order=5)
+        v2 = derivative(f_z, z, dx=0.01*z)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_t(z, t)
 
-        def f_t(t, z, alpha0, alpha1):
+        def f_t(t):
             dist2 = Rayleigh(alpha0, alpha1)
             return dist2.lnpdf(z, t)
-        v2 = derivative(f_t, t, dx=0.01*t, args=(z, alpha0, alpha1), order=5)
+        v2 = derivative(f_t, t, dx=0.01*t)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_alpha0(z, t)
 
-        def f_alpha0(alpha0, t, z, alpha1):
+        def f_alpha0(alpha0):
             dist2 = Rayleigh(alpha0, alpha1)
             return dist2.lnpdf(z, t)
-        v2 = derivative(f_alpha0, alpha0, dx=0.01*alpha0,
-                        args=(t, z, alpha1), order=5)
+        v2 = derivative(f_alpha0, alpha0, dx=0.01*alpha0)
         assert abs(v1/v2-1) < 0.01
 
         v1 = dist.lnpdf_grad_alpha1(z, t)
 
-        def f_alpha1(alpha1, t, z, alpha0):
+        def f_alpha1(alpha1):
             dist2 = Rayleigh(alpha0, alpha1)
             return dist2.lnpdf(z, t)
-        v2 = derivative(f_alpha1, alpha1, dx=0.01*alpha1,
-                        args=(t, z, alpha0), order=5)
+        v2 = derivative(f_alpha1, alpha1, dx=0.01*alpha1)
         assert abs(v1/v2-1) < 0.01
