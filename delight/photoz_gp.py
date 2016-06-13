Fixed mean function (missed normalization)

delight/photoz_gp.py
@@ -6,6 +6,7 @@ from GPy.core.model import Model
 from GPy.likelihoods.gaussian import HeteroscedasticGaussian
 from GPy.inference.latent_function_inference import exact_gaussian_inference
 from GPy.core.parameterization.param import Param
+from GPy.plotting.gpy_plot.gp_plots import plot
 from paramz import ObsAr
 
 from delight.photoz_kernels import Photoz_mean_function, Photoz_kernel
@@ -234,29 +235,106 @@ class PhotozGP(Model):
     def log_likelihood(self):
         return self._log_marginal_likelihood
 
-    def _raw_predict(self, Xnew, full_cov=False):
+    def _raw_predict(self, Xnew, full_cov=False, kern=None):
         """
         For making predictions, without normalization or likelihood
         """
         mu, var = self.posterior._raw_predict(
-            kern=self.kern, Xnew=Xnew, pred_var=self.X, full_cov=full_cov)
+            kern=self.kern if kern is None else kern,
+            Xnew=Xnew, pred_var=self.X, full_cov=full_cov)
         mu += self.mean_function.f(Xnew)
         return mu, var
 
     def log_predictive_density(self, x_test, y_test, Y_metadata=None):
         """
         Calculation of the log predictive density
-
-        .. math:
-            p(y_{*}|D) = p(y_{*}|f_{*})p(f_{*}|\mu_{*}\\sigma^{2}_{*})
-
-        :param x_test: test locations (x_{*})
-        :type x_test: (Nx1) array
-        :param y_test: test observations (y_{*})
-        :type y_test: (Nx1) array
-        :param Y_metadata: metadata associated with the test points
         """
         mu_star, var_star = self._raw_predict(x_test)
         return self.likelihood.log_predictive_density(y_test, mu_star,
                                                       var_star,
                                                       Y_metadata=Y_metadata)
+
+    def plot_f(self, plot_limits=None, fixed_inputs=None,
+               resolution=None,
+               apply_link=False,
+               which_data_ycols='all', which_data_rows='all',
+               visible_dims=None,
+               levels=20, samples=0, lower=2.5, upper=97.5,
+               plot_density=False,
+               plot_data=True, plot_inducing=True,
+               projection='2d', legend=True,
+               predict_kw=None,
+               **kwargs):
+        """
+        Convenience function for plotting the fit of a GP.
+        """
+        return plot(self, plot_limits, fixed_inputs, resolution, True,
+                    apply_link, which_data_ycols, which_data_rows,
+                    visible_dims, levels, samples, 0,
+                    lower, upper, plot_data, plot_inducing,
+                    plot_density, predict_kw, projection, legend, **kwargs)
+
+    def predict(self, Xnew, full_cov=False, Y_metadata=None, kern=None,
+                likelihood=None, include_likelihood=True):
+        mu, var = self._raw_predict(Xnew, full_cov=full_cov, kern=kern)
+        if include_likelihood:
+            if likelihood is None:
+                likelihood = self.likelihood
+            mu, var = likelihood.predictive_values(mu, var,
+                                                   full_cov,
+                                                   Y_metadata=Y_metadata)
+        return mu, var
+
+    def predict_quantiles(self, X, quantiles=(2.5, 97.5), Y_metadata=None,
+                          kern=None, likelihood=None):
+        """
+        Get the predictive quantiles around the prediction at X
+        """
+        m, v = self._raw_predict(X,  full_cov=False, kern=kern)
+        if likelihood is None:
+            likelihood = self.likelihood
+        return likelihood.predictive_quantiles(m, v, quantiles,
+                                               Y_metadata=Y_metadata)
+
+    def posterior_samples_f(self, X, size=10, full_cov=True, **predict_kwargs):
+        """
+        Samples the posterior GP at the points X.
+        """
+        m, v = self._raw_predict(X,  full_cov=full_cov, **predict_kwargs)
+
+        def sim_one_dim(m, v):
+            if not full_cov:
+                return np.random.multivariate_normal(m.flatten(),
+                                                     np.diag(v.flatten()),
+                                                     size).T
+            else:
+                return np.random.multivariate_normal(m.flatten(), v, size).T
+
+        if self.output_dim == 1:
+            return sim_one_dim(m, v)
+        else:
+            fsim = np.empty((self.output_dim, self.num_data, size))
+            for d in range(self.output_dim):
+                if full_cov and v.ndim == 3:
+                    fsim[d] = sim_one_dim(m[:, d], v[:, :, d])
+                elif (not full_cov) and v.ndim == 2:
+                    fsim[d] = sim_one_dim(m[:, d], v[:, d])
+                else:
+                    fsim[d] = sim_one_dim(m[:, d], v)
+        return fsim
+
+    def posterior_samples(self, X, size=10, full_cov=False, Y_metadata=None,
+                          likelihood=None, **predict_kwargs):
+        """
+        Samples the posterior GP at the points X.
+        """
+        fsim = self.posterior_samples_f(X, size, full_cov=full_cov,
+                                        **predict_kwargs)
+        if likelihood is None:
+            likelihood = self.likelihood
+        if fsim.ndim == 3:
+            for d in range(fsim.shape[0]):
+                fsim[d] = likelihood.samples(fsim[d], Y_metadata=Y_metadata)
+        else:
+            fsim = likelihood.samples(fsim, Y_metadata=Y_metadata)
+        return fsim

delight/photoz_kernels.py
@@ -18,9 +18,10 @@ from delight.utils import approx_DL
 
 class Photoz_mean_function(Mapping):
     """
-    Mean function of photoz GP.
+    Mean function of photoz GP
     """
-    def __init__(self, g_AB=1.0, DL_z=None, name='photoz'):
+    def __init__(self, fcoefs_amp, fcoefs_mu, fcoefs_sig,
+                 g_AB=1.0, DL_z=None, name='photoz'):
         """ Constructor."""
         # Call standard Kern constructor with 2 dimensions (z and l).
         super(Photoz_mean_function, self).__init__(4, 1, name)
@@ -31,17 +32,23 @@ class Photoz_mean_function(Mapping):
             self.DL_z = DL_z
         self.g_AB = g_AB
         self.fourpi = 4 * np.pi
+        self.fcoefs_amp = np.array(fcoefs_amp)
+        self.fcoefs_mu = np.array(fcoefs_mu)
+        self.fcoefs_sig = np.array(fcoefs_sig)
+        self.numCoefs = fcoefs_amp.shape[1]
+        self.norms = np.sqrt(2*np.pi)\
+            * np.sum(self.fcoefs_amp * self.fcoefs_sig, axis=1)
 
     def f(self, X):
-        b = X[:, 0]
+        b = X[:, 0].astype(int)
         z = X[:, 1]
         t = X[:, 3]
         l = X[:, 2]
-        return ((1 + z) * l / self.fourpi / self.DL_z(z)**2.0 / self.g_AB)\
-            .reshape(-1, 1)
+        return ((1 + z) * l / self.fourpi / self.DL_z(z)**2.0 / self.g_AB /
+                self.norms[b]).reshape(-1, 1)
 
     def gradients_X(self, dL_dF, X):
-        b = X[:, 0]
+        b = X[:, 0].astype(int)
         z = X[:, 1]
         t = X[:, 3]
         l = X[:, 2]
@@ -50,8 +57,9 @@ class Photoz_mean_function(Mapping):
             DLz = self.DL_z(z)
             grad = np.zeros_like(X)
             grad[:, 1] = (l - 2 * l * (1 + z) * dDLdz / DLz)\
-                / DLz**2 / self.g_AB / self.fourpi  # z
-            grad[:, 2] = (1 + z) / self.fourpi / self.DL_z(z)**2.0 / self.g_AB
+                / DLz**2 / self.g_AB / self.fourpi / self.norms[b]  # z
+            grad[:, 2] = (1 + z) / self.fourpi / self.DL_z(z)**2.0 /\
+                self.g_AB / self.norms[b]
             return np.dot(dL_dF, grad)  # ell
         else:
             raise NotImplementedError
@@ -102,6 +110,7 @@ class Photoz_kernel(Kern):
         self.alpha_C.constrain_positive()
         self.alpha_L.constrain_positive()
         self.alpha_T.constrain_positive()
+        # TODO: addd more concrete constraints?
 
     def set_alpha_C(self, alpha_C):
         """Set alpha_C"""
@@ -304,14 +313,15 @@ class Photoz_kernel(Kern):
         np.sum(tempfull, axis=1, out=grad[:, 3])  # t
 
         # TODO: add kernel derivatives with respect to redshift
-        prefac = fz2[None, :]\
-            / (self.fourpi * self.g_AB * self.DL_z(X[:, 1])[:, None] *
-               self.DL_z(X2[:, 1])[None, :])
-        cst = dL_dK * self.var_T * KT * prefac
-        tempfull = (2 * fz1[:, None] - 2 * fz1[:, None]**2 *
-                    self.DL_z.derivative(X[:, 1])[:, None]) * (KC + KL)\
-            + D_alpha_z * fz1[:, None]**2
-        np.sum(cst * tempfull, axis=1, out=grad[:, 1])  # z
+        if False:
+            prefac = fz2[None, :]\
+                / (self.fourpi * self.g_AB * self.DL_z(X[:, 1])[:, None] *
+                   self.DL_z(X2[:, 1])[None, :])
+            cst = dL_dK * self.var_T * KT * prefac
+            tempfull = (2 * fz1[:, None] - 2 * fz1[:, None]**2 *
+                        self.DL_z.derivative(X[:, 1])[:, None]) * (KC + KL)\
+                + D_alpha_z * fz1[:, None]**2
+            np.sum(cst * tempfull, axis=1, out=grad[:, 1])  # z
 
         return grad
 

tests/test_photoz_gp.py
@@ -68,7 +68,7 @@ def create_gp(create_p_ell_t, create_p_z_t, create_p_t):
                          lines_mu, lines_sig, var_T,
                          alpha_C, alpha_L, alpha_T)
 
-    mean_function = Photoz_mean_function()
+    mean_function = Photoz_mean_function(fcoefs_amp, fcoefs_mu, fcoefs_sig)
 
     noisy_fluxes = np.random.uniform(low=0., high=1., size=size)
     flux_variances = np.random.uniform(low=0., high=1., size=size)
@@ -90,7 +90,7 @@ def create_gp(create_p_ell_t, create_p_z_t, create_p_t):
         X_inducing=None,
         fix_inducing_to_mean_prediction=True
         )
-    gp.set_types(types)
+
     return gp
 
 

tests/test_photoz_kernels.py
@@ -9,8 +9,11 @@ from delight.utils import random_X_bztl,\
 
 from delight.photoz_kernels import Photoz_mean_function, Photoz_kernel
 
-
+size = 5
 NREPEAT = 2
+numBands = 5
+numLines = 3
+numCoefs = 5
 relative_accuracy = 0.05
 # TODO: add tests for diagonal gradients of kernel?
 
@@ -20,10 +23,6 @@ def test_kernel_gradients():
     Numerically test the gradients of the kernel with respect to
     hyperparameters using random inputs and hyperparameters.
     """
-    size = 4
-    numBands = 5
-    numLines = 3
-    numCoefs = 5
 
     for i in range(NREPEAT):
         X = random_X_bztl(size, numBands=numBands)
@@ -91,11 +90,6 @@ def test_kernel_gradients_X():
     Numerically test the gradients of the kernel with respect to the inputs
      using random inputs and hyperparameters.
     """
-    size = 3
-    numBands = 5
-    numLines = 0
-    numCoefs = 1
-
     for i in range(NREPEAT):
         X = random_X_bztl(size, numBands=numBands)
         X2 = random_X_bztl(size, numBands=numBands)
@@ -139,11 +133,11 @@ def test_meanfunction_gradients_X():
     Numerically test the gradients of the mean function
     with respect to the inputs using random inputs and hyperparameters.
     """
-    size = 5
-
+    fcoefs_amp, fcoefs_mu, fcoefs_sig \
+        = random_filtercoefs(numBands, numCoefs)
     for i in range(NREPEAT):
         X = random_X_bztl(size)
-        mf = Photoz_mean_function()
+        mf = Photoz_mean_function(fcoefs_amp, fcoefs_mu, fcoefs_sig)
         dL_dK = 1.0
         v1mat = mf.gradients_X(dL_dK, X)
         v2mat = np.zeros_like(v1mat)
@@ -164,9 +158,10 @@ def test_meanfunction():
     """
     Other tests of the mean function
     """
-    size = 5
+    fcoefs_amp, fcoefs_mu, fcoefs_sig \
+        = random_filtercoefs(numBands, numCoefs)
 
     for i in range(NREPEAT):
         X = random_X_bztl(size)
-        mf = Photoz_mean_function()
+        mf = Photoz_mean_function(fcoefs_amp, fcoefs_mu, fcoefs_sig)
         assert mf.f(X).shape == (size, 1)