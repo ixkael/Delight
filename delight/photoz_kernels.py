Fixed mean function gradients and various improvements

delight/photoz_kernels.py
@@ -48,6 +48,26 @@ class Photoz_mean_function(Mapping):
         self.beta.constrain_positive()
         self.link_parameter(self.beta)
 
+    def set_alpha(self, alpha):
+        """Set alpha"""
+        self.update_model(False)
+        index = self.alpha._parent_index_
+        self.unlink_parameter(self.alpha)
+        self.alpha = Param('alpha', float(alpha))
+        self.link_parameter(self.alpha, index=index)
+        self.alpha.constrain_positive()
+        self.update_model(True)
+
+    def set_beta(self, beta):
+        """Set beta"""
+        self.update_model(False)
+        index = self.beta._parent_index_
+        self.unlink_parameter(self.beta)
+        self.beta = Param('beta', float(beta))
+        self.link_parameter(self.beta, index=index)
+        self.beta.constrain_positive()
+        self.update_model(True)
+
     def f(self, X):
         b = X[:, 0].astype(int)
         z = X[:, 1]
@@ -125,8 +145,10 @@ class Photoz_mean_function(Mapping):
             sum_beta += amp * (1 + erf(term1)) * np.exp(term2) * Dterm2 *\
                 self.sqrthalfpi * sig
 
-        self.alpha.gradient = np.sum(dL_dF * opz * l * fac * sum_alpha)
-        self.beta.gradient = np.sum(dL_dF * opz * l * fac * sum_beta)
+        self.alpha.gradient = np.dot(dL_dF.T, opz * l * fac * sum_alpha)
+        ind = t > 0
+        if ind.sum() > 0:
+            self.beta.gradient = np.dot(dL_dF[ind].T, (opz * l * fac * sum_beta)[ind])
 
 
 class Photoz_kernel(Kern):

delight/utils.py
@@ -26,7 +26,7 @@ def random_X_bzlt(size, numTypes=8, numBands=5, redshiftMax=3.0):
     X[:, 0] = np.random.randint(low=0, high=numBands-1, size=size)
     X[:, 1] = np.random.uniform(low=0, high=redshiftMax, size=size)
     X[:, 3] = np.random.uniform(low=0, high=numTypes-1, size=size)\
-        / float(numTypes)
+        / float(numTypes-1)
     X[:, 2] = np.random.uniform(low=1.0, high=10.0, size=size)
     return X
 

tests/test_photoz_gp.py
@@ -38,7 +38,7 @@ def create_p_ell_t(request):
         return None
     else:
         ellStar = np.random.uniform(0., 10., size=1)
-        alpha0, alpha1 = np.random.uniform(-1., 0., size=2)
+        alpha0, alpha1 = np.random.uniform(-0.9, -0.1, size=2)
         return Schechter(ellStar, alpha0, alpha1)
 
 
@@ -101,6 +101,26 @@ def test_gradients(create_gp):
     """Test all gradients of the full likelihood function"""
     gp = create_gp
     assert(isinstance(gp, PhotozGP))
+
+    v1 = gp.mean_function.alpha.gradient
+
+    def f_alpha(v):
+        gp2 = deepcopy(gp)
+        gp2.mean_function.set_alpha(v)
+        return gp2._log_marginal_likelihood
+    v2 = derivative(f_alpha, gp.mean_function.alpha.values,
+                    dx=0.01*gp.mean_function.alpha.values)
+    assert abs(v1/v2-1) < relative_accuracy
+
+    v1 = gp.mean_function.beta.gradient
+
+    def f_beta(v):
+        gp2 = deepcopy(gp)
+        gp2.mean_function.set_beta(v)
+        return gp2._log_marginal_likelihood
+    v2 = derivative(f_beta, gp.mean_function.beta.values,
+                    dx=0.01*gp.mean_function.beta.values)
+    assert abs(v1/v2-1) < relative_accuracy
 
     v1 = gp.kern.alpha_L.gradient
 
@@ -276,3 +296,5 @@ def test_hmc(create_gp):
 
     hmc = GPy.inference.mcmc.HMC(gp, stepsize=1e-2)
     s = hmc.sample(num_samples=3)
+
+# TODO: test covariance is semi positive definite

tests/test_photoz_kernels.py
@@ -9,7 +9,7 @@ from delight.utils import random_X_bzlt,\
 
 from delight.photoz_kernels import Photoz_mean_function, Photoz_kernel
 
-size = 4
+size = 6
 NREPEAT = 4
 numBands = 5
 numLines = 10
@@ -152,20 +152,21 @@ def test_meanfunction_gradients_X():
     fcoefs_amp, fcoefs_mu, fcoefs_sig \
         = random_filtercoefs(numBands, numCoefs)
     for i in range(NREPEAT):
-        alpha = np.random.uniform(low=0, high=1e-4, size=1)
+        alpha = np.random.uniform(low=0, high=2e-3, size=1)
         beta = np.random.uniform(low=1, high=3, size=1)
         X = random_X_bzlt(size)
         mf = Photoz_mean_function(alpha, beta,
                                   fcoefs_amp, fcoefs_mu, fcoefs_sig)
-        dL_dK = 1.0
-        mf.update_gradients(dL_dK, X)
+        dL_dm = np.ones((size,))
+        dL_dK = np.ones((size, size))
+        mf.update_gradients(dL_dm, X)
 
         v1 = mf.alpha.gradient
 
         def f_alpha(alpha):
-            mf = Photoz_mean_function(alpha, beta,
-                                      fcoefs_amp, fcoefs_mu, fcoefs_sig)
-            return np.sum(mf.f(X))
+            mf2 = Photoz_mean_function(alpha, beta,
+                                       fcoefs_amp, fcoefs_mu, fcoefs_sig)
+            return np.sum(mf2.f(X))
 
         v2 = derivative(f_alpha, alpha, dx=0.01*alpha)
         assert abs(v1/v2-1) < relative_accuracy
@@ -173,14 +174,15 @@ def test_meanfunction_gradients_X():
         v1 = mf.beta.gradient
 
         def f_beta(beta):
-            mf = Photoz_mean_function(alpha, beta,
-                                      fcoefs_amp, fcoefs_mu, fcoefs_sig)
-            return np.sum(mf.f(X))
+            mf2 = Photoz_mean_function(alpha, beta,
+                                       fcoefs_amp, fcoefs_mu, fcoefs_sig)
+            return np.sum(mf2.f(X))
 
-        v2 = derivative(f_beta, beta, dx=0.01*alpha)
+        v2 = derivative(f_beta, beta, dx=0.01*beta)
         assert abs(v1/v2-1) < relative_accuracy
 
         v1mat = mf.gradients_X(dL_dK, X)
+        print v1mat.shape, X.shape
         v2mat = np.zeros_like(v1mat)
         for dim in [2]:  # Order: b z l t.
             for k1 in range(size):
@@ -202,7 +204,7 @@ def test_meanfunction():
     fcoefs_amp, fcoefs_mu, fcoefs_sig \
         = random_filtercoefs(numBands, numCoefs)
     for i in range(NREPEAT):
-        alpha = np.random.uniform(low=0, high=1e-4, size=1)
+        alpha = np.random.uniform(low=0, high=2e-3, size=1)
         beta = np.random.uniform(low=1., high=3., size=1)
         X = random_X_bzlt(size)
         bands, redshifts, luminosities, types = np.split(X, 4, axis=1)