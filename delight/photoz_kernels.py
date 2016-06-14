Fixed type and lum gradients

delight/photoz_kernels.py
@@ -89,20 +89,48 @@ class Photoz_mean_function(Mapping):
         return (fac * thesum).reshape((-1, 1))
 
     def gradients_X(self, dL_dF, X):
+        grad = np.zeros_like(X)
         b = X[:, 0].astype(int)
         z = X[:, 1]
         t = X[:, 3]
         l = X[:, 2]
+        opz = 1 + z
+        fac = opz/self.fourpi/self.DL_z(z)**2.0/self.g_AB/self.norms[b]
+        sum_t = np.zeros_like(t)
+        sum_ell = np.zeros_like(t)
+        for i in range(self.numCoefs):
+            amp, mu, sig = self.fcoefs_amp[b, i],\
+                           self.fcoefs_mu[b, i],\
+                           self.fcoefs_sig[b, i]
+            alphat = self.alpha * t**self.beta
+            term1 = (mu * opz - alphat * sig**2) /\
+                (1.41421356237 * sig * opz)
+            term2 = alphat * (self.lambdaRef - mu/opz + alphat*(sig/opz)**2/2)
+            sum_ell += amp * (1 + erf(term1)) * np.exp(term2) *\
+                self.sqrthalfpi * sig
+
+            Dterm1 = (self.alpha*self.beta *
+                      np.exp(-((-self.alpha*sig**2*t**self.beta + mu*opz)**2 /
+                               (2*sig**2*opz**2))) *
+                      self.sqrthalfpi*sig*t**(self.beta-1)) / opz
+            Dterm2 = ((self.alpha**2*self.beta*sig**2*t**(2*self.beta-1)) /
+                      (2*opz**2) +
+                      self.alpha*self.beta*t**(self.beta-1) *
+                      (self.lambdaRef +
+                       (self.alpha*sig**2*t**self.beta)/(2*opz**2) - mu/opz))
+            sum_t += amp * np.exp(term2) * Dterm1 *\
+                self.sqrthalfpi * sig
+            sum_t += amp * (1 + erf(term1)) * np.exp(term2) * Dterm2 *\
+                self.sqrthalfpi * sig
+
+        grad[:, 2] = fac * sum_ell  # ell
+        grad[:, 3] = l * fac * sum_t  # t
         if isinstance(self.DL_z, approx_DL):
             dDLdz = self.DL_z.derivative(z)
-            DLz = self.DL_z(z)
-            grad = np.zeros_like(X)
-            # TODO: implement t gradient
             # TODO: implement z gradient
-            grad[:, 2] = self.f(X).flatten() / l  # ell
-            return np.dot(dL_dF, grad)
         else:
             raise NotImplementedError
+        return np.dot(dL_dF, grad)
 
     def update_gradients(self, dL_dF, X):
         b = X[:, 0].astype(int)

tests/test_photoz_gp.py
@@ -13,8 +13,8 @@ from delight.utils import random_X_bzlt,\
 from scipy.misc import derivative
 from copy import deepcopy
 
-NREPEAT = 1
-size = 2
+NREPEAT = 3
+size = 7
 numBands = 5
 numLines = 3
 numCoefs = 3
@@ -130,7 +130,7 @@ def test_gradients(create_gp):
         return gp2._log_marginal_likelihood
     v2 = derivative(f_alpha_L, gp.kern.alpha_L.values,
                     dx=0.01*gp.kern.alpha_L.values)
-    if np.abs(v1) > 1e-12 and np.abs(v2) > 1e-12:
+    if np.abs(v1) > 1e-10 and np.abs(v2) > 1e-10:
         assert abs(v1/v2-1) < relative_accuracy
 
     v1 = gp.kern.alpha_C.gradient
@@ -161,7 +161,7 @@ def test_gradients(create_gp):
         return gp2._log_marginal_likelihood
     v2 = derivative(f_var_L, gp.kern.var_L.values,
                     dx=0.01*gp.kern.var_L.values)
-    if np.abs(v1) > 1e-12 and np.abs(v2) > 1e-12:
+    if np.abs(v1) > 1e-10 and np.abs(v2) > 1e-10:
         assert abs(v1/v2-1) < relative_accuracy
 
     v1 = gp.kern.alpha_T.gradient
@@ -172,7 +172,7 @@ def test_gradients(create_gp):
         return gp2._log_marginal_likelihood
     v2 = derivative(f_alpha_T, gp.kern.alpha_T.values,
                     dx=0.01*gp.kern.alpha_T.values)
-    if np.abs(v1) > 1e-12 and np.abs(v2) > 1e-12:
+    if np.abs(v1) > 1e-10 and np.abs(v2) > 1e-10:
         assert abs(v1/v2-1) < relative_accuracy
 
     if gp.prior_z_t is not None:

tests/test_photoz_kernels.py
@@ -16,6 +16,7 @@ numLines = 10
 numCoefs = 10
 relative_accuracy = 0.05
 # TODO: add tests for diagonal gradients of kernel?
+# TODO: add formal/numerical test for kernel w.r.t. mean fct
 
 
 def test_kernel_gradients():
@@ -37,7 +38,7 @@ def test_kernel_gradients():
         gp = Photoz_kernel(fcoefs_amp, fcoefs_mu, fcoefs_sig,
                            lines_mu, lines_sig, var_C, var_L,
                            alpha_C, alpha_L, alpha_T)
-        dL_dK = 1.0
+        dL_dK = np.diag(dL_dm)
         gp.update_gradients_full(dL_dK, X, X2)
 
         v1 = gp.alpha_T.gradient
@@ -140,8 +141,7 @@ def test_kernel_gradients():
 
                 v2mat[k1, dim] \
                     = derivative(f_x, X[k1, dim], dx=0.01*X[k1, dim])
-
-            np.allclose(v1mat[:, dim], v2mat[:, dim], rtol=relative_accuracy)
+                np.allclose(v1mat[k1, dim], v2mat[k1, dim])
 
 
 def test_meanfunction_gradients_X():
@@ -158,7 +158,7 @@ def test_meanfunction_gradients_X():
         mf = Photoz_mean_function(alpha, beta,
                                   fcoefs_amp, fcoefs_mu, fcoefs_sig)
         dL_dm = np.ones((size,))
-        dL_dK = np.ones((size, size))
+        dL_dK = np.diag(dL_dm)
         mf.update_gradients(dL_dm, X)
 
         v1 = mf.alpha.gradient
@@ -182,9 +182,9 @@ def test_meanfunction_gradients_X():
         assert abs(v1/v2-1) < relative_accuracy
 
         v1mat = mf.gradients_X(dL_dK, X)
-        print v1mat.shape, X.shape
         v2mat = np.zeros_like(v1mat)
-        for dim in [2]:  # Order: b z l t.
+        for dim in [2, 3]:  # Order: b z l t.
+            print dim
             for k1 in range(size):
                 def f_x(x1):
                     Xb = 1*X
@@ -193,8 +193,7 @@ def test_meanfunction_gradients_X():
 
                 v2mat[k1, dim] \
                     = derivative(f_x, X[k1, dim], dx=0.01*X[k1, dim])
-
-            np.allclose(v1mat[:, dim], v2mat[:, dim], rtol=relative_accuracy)
+                assert abs(v1mat[k1, dim]/v2mat[k1, dim]-1) < relative_accuracy
 
 
 def test_meanfunction():