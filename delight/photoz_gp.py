Fixed silly sign error in Schechter lum fct prior

delight/photoz_gp.py
@@ -108,7 +108,7 @@ class PhotozGP(Model):
         }
 
         self.extranoise = Param('extranoise', float(extranoise))
-        self.extranoise.constrain_positive()
+        self.extranoise.constrain_bounded(1e-12, 1e-4)
         self.link_parameter(self.extranoise)
         self.likelihood = HeteroscedasticGaussian(self.Y_metadata)
         self.flux_variances = flux_variances.T.reshape((-1, 1))
@@ -161,6 +161,22 @@ class PhotozGP(Model):
         for nmb, (idx1, idx2) in array_params.iteritems():
             array_params[nmb] = (np.array(idx1), np.array(idx2))
         return scalar_params, array_params
+
+    def set_X_inducing(self, X_inducing):
+        self.X_inducing = X_inducing
+
+        assert self.X_inducing.shape[1] == self.input_dim
+        self.Y_inducing_mean = np.zeros((self.X_inducing.shape[0], 1))
+        self.Y_inducing_std = np.zeros((self.X_inducing.shape[0], 1))
+        self.derived_params.append(self.Y_inducing_mean)
+        self.derived_param_names.append('Y_inducing_mean')
+        self.derived_params.append(self.Y_inducing_std)
+        self.derived_param_names.append('Y_inducing_std')
+
+        mu, var = self._raw_predict(  # marglike, dL_dKz, V
+            self.X_inducing, full_cov=False, marglike=False)
+        self.Y_inducing_mean[:, 0] = mu[:, 0]
+        self.Y_inducing_std[:, 0] = np.sqrt(var[:, 0])  # np.sqrt(np.diag(var))
 
     def set_unfixed_parameters(self, params, scalar_params, array_params):
         """Set unfixed parameters all at once"""
@@ -262,10 +278,11 @@ class PhotozGP(Model):
 
         if self.X_inducing is not None:
 
-            mu, var, marglike, dL_dKz, V = self._raw_predict(
-                self.X_inducing, full_cov=True, marglike=True)
+            mu, var = self._raw_predict(  # marglike, dL_dKz, V
+                self.X_inducing, full_cov=False, marglike=False)
             self.Y_inducing_mean[:, 0] = mu[:, 0]
-            self.Y_inducing_std[:, 0] = np.sqrt(np.diag(var))
+            self.Y_inducing_std[:, 0] = np.sqrt(var[:, 0])
+            # np.sqrt(np.diag(var))
 
             if False:
                 self._log_marginal_likelihood += marglike
@@ -388,7 +405,7 @@ class PhotozGP(Model):
         else:
             Kxx = kern.Kdiag(Xnew)
             if self.posterior._woodbury_chol.ndim == 2:
-                tmp = dtrtrs(self._woodbury_chol, Kx)[0]
+                tmp = dtrtrs(self.posterior._woodbury_chol, Kx)[0]
                 var = (Kxx - np.square(tmp).sum(0))[:, None]
             elif self.posterior._woodbury_chol.ndim == 3:  # Missing data
                 var = np.empty((Kxx.shape[0],

delight/photoz_kernels.py
@@ -35,7 +35,7 @@ class Photoz_mean_function(Mapping):
         else:
             self.DL_z = DL_z
         self.g_AB = g_AB
-        assert lambdaRef > 1 and lambdaRef < 1e5
+        assert lambdaRef > 1e2 and lambdaRef < 1e5
         self.lambdaRef = lambdaRef
         self.fourpi = 4 * np.pi
         self.sqrthalfpi = np.sqrt(np.pi/2)
@@ -113,7 +113,7 @@ class Photoz_mean_function(Mapping):
         t = X[:, 3]
         opz = 1. + z
         self.update_parts(X)
-        fac = l*opz/self.fourpi/self.DL_z(z)**2.0/self.g_AB/self.norms[b]
+        fac = l*opz**2/self.fourpi/self.DL_z(z)**2.0/self.g_AB/self.norms[b]
         return (fac * self.sum_mf).reshape((-1, 1))
 
     def get_gradients_X(self, X):
@@ -124,7 +124,7 @@ class Photoz_mean_function(Mapping):
         t = X[:, 3]
         opz = 1 + z
         self.update_parts(X)
-        fac = opz/self.fourpi/self.DL_z(z)**2.0/self.g_AB/self.norms[b]
+        fac = opz**2/self.fourpi/self.DL_z(z)**2.0/self.g_AB/self.norms[b]
         grad_ell = fac * self.sum_ell  # ell
         grad_t = l * fac * self.sum_t  # t
         # TODO: implement z gradient
@@ -193,13 +193,13 @@ class Photoz_kernel(Kern):
         self.alpha_T.constrain_positive()
         self.link_parameters(self.var_C, self.var_L,
                              self.alpha_C, self.alpha_L, self.alpha_T)
-        self.Thashd = 0
-        self.BZhashd = 0
-        self.Thash = 0
-        self.T2hash = 0
-        self.BZhash = 0
-        self.BZ2hash = 0
-        self.CLhash = 0
+        self.Thashd = None
+        self.BZhashd = None
+        self.Thash = None
+        self.T2hash = None
+        self.BZhash = None
+        self.BZ2hash = None
+        self.CLhash = None
         # TODO: addd more realistic constraints?
 
     def set_alpha_C(self, alpha_C):
@@ -534,7 +534,7 @@ class Photoz_kernel(Kern):
 
             else:  # not use interpolators
                 fz1 = 1 + X[:, 1]
-                kernelparts_diag(self.nz, self.numCoefs, self.numLines,
+                kernelparts_diag(NO1, self.numCoefs, self.numLines,
                                  self.alpha_C, self.alpha_L,
                                  self.fcoefs_amp, self.fcoefs_mu,
                                  self.fcoefs_sig,

delight/priors.py
@@ -66,19 +66,19 @@ class Schechter(Parameterized):
     def pdf(self, ell, t):
         """prob"""
         alpha = self.alpha0 + (self.alpha1 - self.alpha0) * t
-        return (ell / self.ellStar)**alpha * np.exp(ell / self.ellStar)\
+        return (ell / self.ellStar)**alpha * np.exp(-ell / self.ellStar)\
             / self.ellStar / gamma(1 + alpha)
 
     def lnpdf(self, ell, t):
         """minus Lnprob"""
         alpha = self.alpha0 + (self.alpha1 - self.alpha0) * t
         return gammaln(1 + alpha) + (alpha + 1)\
-            * self.lnEllStar - alpha * np.log(ell) - ell/self.ellStar
+            * self.lnEllStar - alpha * np.log(ell) + ell/self.ellStar
 
     def lnpdf_grad_ell(self, ell, t):
         """Derivative of lnprob with respect to ell"""
-        return - 1/self.ellStar - (self.alpha0 +
-                                   (self.alpha1 - self.alpha0) * t) / ell
+        return 1/self.ellStar - (self.alpha0 +
+                                 (self.alpha1 - self.alpha0) * t) / ell
 
     def lnpdf_grad_t(self, ell, t):
         """Derivative of lnprob with respect to t"""
@@ -88,9 +88,9 @@ class Schechter(Parameterized):
 
     def lnpdf_grad_alpha0(self, ell, t):
         """Derivative of lnprob with respect to alpha0"""
-        return (1-t) * (-np.log(ell) + self.lnEllStar +
-                        polygamma(0, 1 + self.alpha0 +
-                        (self.alpha1 - self.alpha0) * t))
+        return - (1-t) * (np.log(ell) - self.lnEllStar -
+                          polygamma(0, 1 + self.alpha0 +
+                          (self.alpha1 - self.alpha0) * t))
 
     def lnpdf_grad_alpha1(self, ell, t):
         """Derivative of lnprob with respect to alpha1"""
@@ -102,7 +102,7 @@ class Schechter(Parameterized):
         """Derivative of lnprob with respect to alpha1"""
         return 1/self.ellStar *\
             (1 + self.alpha0 + (self.alpha1 - self.alpha0) *
-             t + ell/self.ellStar)
+             t - ell/self.ellStar)
 
     def update_gradients(self, dL,  ell, t):
         """Update gradient structures"""

tests/profiling.py
@@ -18,12 +18,13 @@ from delight.photoz_kernels import Photoz_mean_function, Photoz_kernel
 
 NREPEAT = 1
 nObj = 10
-nInducing = 35
+nInducing = 5
 numBands = 5
 size = numBands * nObj
 redshiftGrid = np.linspace(0, 3, num=30)
 use_interpolators = True
 
+extranoise = 1e-8
 numLines = 3
 numCoefs = 10
 bandsUsed = range(numBands)
@@ -117,8 +118,6 @@ fcoefs_amp, fcoefs_mu, fcoefs_sig \
     = random_filtercoefs(numBands, numCoefs)
 lines_mu, lines_sig = random_linecoefs(numLines)
 var_C, var_L, alpha_C, alpha_L, alpha_T = random_hyperparams()
-alpha = np.random.uniform(low=1e-4, high=1e-3, size=1)
-beta = np.random.uniform(low=0.3, high=0.7, size=1)
 kern = Photoz_kernel(fcoefs_amp, fcoefs_mu, fcoefs_sig,
                      lines_mu, lines_sig, var_C, var_L,
                      alpha_C, alpha_L, alpha_T)
@@ -202,7 +201,7 @@ print "=> gradients_X (X fixed): %s s" % (t.secs / NREPEAT)
 
 print '-------'
 
-mf = Photoz_mean_function(alpha, beta, fcoefs_amp, fcoefs_mu, fcoefs_sig)
+mf = Photoz_mean_function(fcoefs_amp, fcoefs_mu, fcoefs_sig)
 
 with Timer() as t:
     for i in range(NREPEAT):
@@ -251,10 +250,10 @@ flux_variances = np.random.uniform(low=0., high=1., size=size)\
 with Timer() as t:
     gp = PhotozGP(
         redshifts, luminosities, types, used_indices,
-        noisy_fluxes, flux_variances, bandsUsed,
+        noisy_fluxes, flux_variances, extranoise, bandsUsed,
         fcoefs_amp, fcoefs_mu, fcoefs_sig,
         lines_mu, lines_sig,
-        alpha, beta, var_C, var_L,
+        var_C, var_L,
         alpha_C, alpha_L, alpha_T,
         X_inducing=X_inducing,
         redshiftGrid=redshiftGrid,
@@ -299,8 +298,6 @@ print "=> HMC iterations (all varying): %s s" % (t.secs / NREPEAT)
 gp.kern.var_L.fix()
 gp.kern.alpha_C.fix()
 gp.kern.alpha_L.fix()
-gp.mean_function.alpha.fix()
-gp.mean_function.beta.fix()
 
 hmc = GPy.inference.mcmc.HMC(gp, stepsize=1e-4)
 with Timer() as t:

tests/test_photoz_gp.py
@@ -16,7 +16,7 @@ from copy import deepcopy
 
 NREPEAT = 1
 nObj = 5
-nInducing = 5
+nInducing = 10
 nObjUnfixed = 4
 numBands = 2
 numLines = 3
@@ -32,7 +32,7 @@ fcoefs_amp, fcoefs_mu, fcoefs_sig \
     = random_filtercoefs(numBands, numCoefs)
 lines_mu, lines_sig = random_linecoefs(numLines)
 var_C, var_L, alpha_C, alpha_L, alpha_T = random_hyperparams()
-noise = np.random.uniform(0, 1)
+noise = np.random.uniform(0, 1e-6)
 
 noisy_fluxes = np.random.uniform(low=0.5, high=1., size=size)\
     .reshape((nObj, numBands))
@@ -83,7 +83,7 @@ def use_inducing(request):
         return random_X_bzlt(nInducing, numBands=numBands)
 
 
-@pytest.fixture(params=[False, True])
+@pytest.fixture(params=[True, False])
 def use_interpolators(request):
     return request.param
 