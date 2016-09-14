eabe196a610182123075def7e3a96127a38c62e8
Boris Leistedt <boris.leistedt@gmail.com>
2016-09-14 19:00:46 +0100

    optional plot

--- scripts/processFilters.py
+++ scripts/processFilters.py
@@ -8,6 +8,7 @@ bandNames = ['u', 'g', 'r', 'i', 'z']  # Bands
 fmt = '.res'
 max_redshift = 2.0  # for plotting purposes
 root = './data/SDSS_FILTERS'
+make_plots = False
 
 
 # Function we will optimize
@@ -44,14 +45,15 @@ for band in bandNames:
     fname_out = root + '/' + band + '_gaussian_coefficients.txt'
     np.savetxt(fname_out, coefs, header=fname_in)
 
-    fig, ax = plt.subplots(figsize=(8, 4))
-    ax.plot(x[ind], y[ind], lw=3, label='True filter', c='k')
-
     xf = np.linspace(lambdaMin, lambdaMax, num=1000)
     yy = 0*xf
     for i in range(numCoefs):
         yy += coefs[i, 0] * np.exp(-0.5*((coefs[i, 1] - xf)/coefs[i, 2])**2.0)
-    ax.plot(xf, yy, lw=2, c='r', label='Gaussian fit')
+
+    if make_plots:
+        fig, ax = plt.subplots(figsize=(8, 4))
+        ax.plot(x[ind], y[ind], lw=3, label='True filter', c='k')
+        ax.plot(xf, yy, lw=2, c='r', label='Gaussian fit')
 
     coefs_redshifted = 1*coefs
     coefs_redshifted[:, 1] /= (1. + max_redshift)
@@ -64,14 +66,17 @@ for band in bandNames:
         yy += coefs_redshifted[i, 0] *\
             np.exp(-0.5*((coefs_redshifted[i, 1] - xf) /
                    coefs_redshifted[i, 2])**2.0)
-    ax.plot(xf, yy, lw=2, c='b', label='G fit at z='+str(max_redshift))
-    title = band + ' band (' + fname_in + ') with %i' % numCoefs+' components'
-    ax.set_title(title)
-    ax.set_ylim([0, data[:, 1].max()*1.2])
-    ax.set_yticks([])
-    ax.set_xlabel('$\lambda$')
-    ax.legend(loc='upper center', frameon=False, ncol=3)
 
-    fig.tight_layout()
-    fname_fig = root + '/' + band + '_gaussian_approximation.png'
-    fig.savefig(fname_fig)
+    if make_plots:
+        ax.plot(xf, yy, lw=2, c='b', label='G fit at z='+str(max_redshift))
+        title = band + ' band (' + fname_in +\
+            ') with %i' % numCoefs+' components'
+        ax.set_title(title)
+        ax.set_ylim([0, data[:, 1].max()*1.2])
+        ax.set_yticks([])
+        ax.set_xlabel('$\lambda$')
+        ax.legend(loc='upper center', frameon=False, ncol=3)
+
+        fig.tight_layout()
+        fname_fig = root + '/' + band + '_gaussian_approximation.png'
+        fig.savefig(fname_fig)
