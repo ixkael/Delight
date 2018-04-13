fb774d3d8c1a4abe2e75b67f6597d0acfddb234a
Boris Leistedt <boris.leistedt@gmail.com>
2018-04-13 17:24:53 +0100

    uploaded scripts

--- scripts/mcmcTemplatePriors.py
+++ scripts/mcmcTemplatePriors.py
@@ -9,43 +9,9 @@ from delight.utils import *
 from delight.photoz_gp import PhotozGP
 from delight.photoz_kernels import Photoz_mean_function, Photoz_kernel
 import scipy.stats
-from scipy.special import logsumexp
 import matplotlib.pyplot as plt
 import emcee
 import corner
-
-def approx_flux_likelihood_multiobj(
-        f_obs,  # no, nf
-        f_obs_var,  # no, nf
-        f_mod,  # no, nt, nf
-        ell_hat,  # 1
-        ell_var,  # 1
-        returnChi2=False,
-        normalized=True):
-
-    assert len(f_obs.shape) == 2
-    assert len(f_obs_var.shape) == 2
-    assert len(f_mod.shape) == 3
-    no, nt, nf = f_mod.shape
-    f_obs_r = f_obs[:, None, :]
-    var = f_obs_var[:, None, :]
-    invvar = np.where(f_obs_r/var < 1e-6, 0.0, var**-1.0)  # nz * nt * nf
-    FOT = np.sum(f_mod * f_obs_r * invvar, axis=2)\
-        + ell_hat / ell_var  # no * nt
-    FTT = np.sum(f_mod**2 * invvar, axis=2)\
-        + 1. / ell_var  # no * nt
-    FOO = np.sum(f_obs_r**2 * invvar, axis=2)\
-        + ell_hat**2 / ell_var  # no * nt
-    sigma_det = np.prod(var, axis=2)
-    chi2 = FOO - FOT**2.0 / FTT  # no * nt
-    denom = np.sqrt(FTT)
-    if normalized:
-        denom *= np.sqrt(sigma_det * (2*np.pi)**nf * ell_var)
-    like = np.exp(-0.5*chi2) / denom  # no * nt
-    if returnChi2:
-        return chi2
-    else:
-        return like
 
 
 comm = MPI.COMM_WORLD
@@ -99,44 +65,23 @@ for z, ell, bands, fluxes, fluxesVar, bCV, fCV, fvCV in trainingDataIter:
             fmod_atZ[loc, t, ib] = ell * np.interp(z, redshiftGrid,
                                                    f_mod[:, t, b])
 
-def scalefree_flux_lnlikelihood_multiobj(
-        f_obs,  # no, ..., nf
-        f_obs_var,  # no, ..., nf
-        f_mod,  # ..., nf
-        normalized=True):
-
-    assert len(f_obs.shape) == len(f_mod.shape)
-    assert len(f_obs_var.shape) == len(f_mod.shape)
-    assert len(f_mod.shape) >= 2
-    nf = f_mod.shape[-1]
-    assert f_obs.shape[-1] == nf
-    assert f_obs_var.shape[-1] == nf
-    # nz * nt * nf
-    invvar = np.where(f_obs/f_obs_var < 1e-6, 0.0, f_obs_var**-1.0)
-    FOT = np.sum(f_mod * f_obs * invvar, axis=-1)  # no * nt
-    FTT = np.sum(f_mod**2 * invvar, axis=-1)  # no * nt
-    FOO = np.sum(f_obs**2 * invvar, axis=-1)  # no * nt
-    logsigma_det = np.sum(np.log(f_obs_var), axis=-1)
-    chi2 = FOO - FOT**2.0 / FTT  # no * nt
-    logdenom = 0.5 * np.log(FTT)
-    ellML = FOT / FTT
-    if normalized:
-        logdenom += 0.5* logsigma_det
-    lnlike = -0.5*chi2 - logdenom  # no * nt
-    return lnlike, ellML
 
 def lnprob(params, nt, allFluxes, allFluxesVar, fmod_atZ, pmin, pmax):
     if np.any(params > pmax) or np.any(params < pmin):
             return - np.inf
     alphas = params[0:nt]
     betas = params[nt:2*nt][None, :]
-    lnlike_grid = scalefree_flux_lnlikelihood_multiobj(
-        allFluxes[:, None, :], allFluxesVar[:, None, :], fmod_atZ)  # no, nt
+    sigma_ell = 1e16
+    like_grid = approx_flux_likelihood_multiobj(
+        allFluxes, allFluxesVar, fmod_atZ, 1, sigma_ell**2.)  # no, nt
     p_t = dirichlet(alphas)
     p_z = redshifts * np.exp(-0.5 * redshifts**2 / betas) / betas  # p(z|t)
     p_z_t = p_z * p_t  # no, nt
-    lnlike_lt = logsumexp(lnlike_grid + np.log(p_z_t), axis=1)
-    return - np.sum(lnlike_lt)
+    like_lt = (like_grid * p_z_t).sum(axis=1)
+    eps = 1e-305
+    ind = like_lt > eps
+    theprob = np.log(like_lt[ind]).sum()
+    return theprob
 
 
 def plot_params(params):
@@ -166,30 +111,53 @@ def plot_params(params):
 
 pmin = np.concatenate((np.repeat(0., nt), np.repeat(0., nt)))
 pmax = np.concatenate((np.repeat(200., nt), np.repeat(redshiftGrid[-1], nt)))
-ndim, nwalkers = 2*nt, 1000
+
+ndim, nwalkers = 2*nt, 100
 p0 = [pmin + (pmax-pmin)*np.random.uniform(0, 1, size=ndim)
       for i in range(nwalkers)]
 
-bounds = [[pmin[i], pmax[i]] for i in range(ndim)]
+for i in range(10):
+    print(lnprob(p0[i], nt, allFluxes, allFluxesVar, fmod_atZ, pmin, pmax))
 
-vals = np.zeros((len(p0), ))
-for i in range(len(p0)):
-    vals[i] = lnprob(p0[i], nt, allFluxes, allFluxesVar, fmod_atZ, pmin, pmax)
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob,
+                                threads=4,
+                                args=[nt, allFluxes, allFluxesVar, fmod_atZ,
+                                      pmin, pmax])
+pos, prob, state = sampler.run_mcmc(p0, 2)
+sampler.reset()
+sampler.run_mcmc(pos, 10)
+print("Mean acceptance fraction: {0:.3f}"
+      .format(np.mean(sampler.acceptance_fraction)))
+samples = sampler.chain.reshape((-1, ndim))
+lnprob = sampler.lnprobability.reshape((-1, 1))
 
-loc = np.argmin(vals)
-print('Minimum value:', vals[loc])
+params_mean = samples.mean(axis=0)
+params_std = samples.std(axis=0)
 
-res = minimize(lnprob, p0[loc], args=(nt, allFluxes, allFluxesVar, fmod_atZ, pmin, pmax), bounds=bounds)
+fig, axs = plt.subplots(4, 5, figsize=(16, 8))
+axs = axs.ravel()
+for i in range(ndim):
+    axs[i].hist(samples[:, i], 50, color="k", histtype="step")
+    axs[i].axvspan(params_mean[i]-params_std[i],
+                   params_mean[i]+params_std[i], color='gray', alpha=0.5)
+    axs[i].axvline(params_mean[i], c='k', lw=2)
+fig.tight_layout()
+fig.savefig('prior_parameters.pdf')
 
-print(res)
+fig = plot_params(params_mean)
+fig.savefig('prior_meanparameters.pdf')
 
-params_mean = res.x
+print("params_mean", params_mean)
+print("params_std", params_std)
 
 alphas = params_mean[0:nt]
 betas = params_mean[nt:2*nt]
 
 alpha0 = np.sum(alphas)
 print("p_t:", ' '.join(['%.2g' % x for x in alphas / alpha0]))
-print("p_z_t:", ' '.join(['%.2g' % x for x in betas]))
 print("p_t err:", ' '.join(['%.2g' % x
       for x in np.sqrt(alphas*(alpha0-alphas)/alpha0**2/(alpha0+1))]))
+print("p_z_t:", ' '.join(['%.2g' % x for x in betas]))
+
+fig = corner.corner(samples)
+fig.savefig("triangle.pdf")
