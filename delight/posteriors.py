508dca2087627b578507571592b00e94b13d5371
Dagoret Campagne Sylvie <dagoret@lal.in2p3.fr>
2021-10-20 12:52:33 +0200

    correct a library python

--- delight/posteriors.py
+++ delight/posteriors.py
@@ -2,8 +2,8 @@
 
 import numpy as np
 
-from scipy.misc import logsumexp
-
+#from scipy.misc import logsumexp
+from scipy.special import logsumexp
 
 def hypercube2simplex(zs):
     fac = np.concatenate((1 - zs, np.array([1])))
